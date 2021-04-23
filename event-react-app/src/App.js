aed74f1702ee614c5fccb877529044910b2d9fb1
vladdominator <https://github.com/vladdominator>
2021-04-23 13:17:29 +0300

    remove image

--- event-react-app/src/App.js
+++ event-react-app/src/App.js
@@ -1,4 +1,3 @@
-import food from './food.jpg'
 import './App.css';
 import React from 'react';
 import WeeklyScheduler from './components/Monthly';
@@ -62,7 +61,6 @@ function App() {
             <Route path="/">
               <h1>Welcome to RedPeas Weather</h1>
               <p>Welcome to ReadPeas. Please see now <Link to="/weather"> weather.</Link></p>
-              <img className='food' src={food} />
             </Route>
 
             <Route path="*">
