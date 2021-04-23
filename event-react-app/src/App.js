67d73f3ecc953aa025b6aa3e98a12620e4241f7d
vladdominator <https://github.com/vladdominator>
2021-04-23 13:12:20 +0300

    change projects

--- event-react-app/src/App.js
+++ event-react-app/src/App.js
@@ -1,4 +1,4 @@
-import food from './food.jpg'
+import weather from './weather.jpg'
 import './App.css';
 import React from 'react';
 import WeeklyScheduler from './components/Monthly';
@@ -62,7 +62,7 @@ function App() {
             <Route path="/">
               <h1>Welcome to RedPeas Weather</h1>
               <p>Welcome to ReadPeas. Please see now <Link to="/weather"> weather.</Link></p>
-              <img className='food' src={food} />
+              <img className='weather' src={weather} />
             </Route>
 
             <Route path="*">
